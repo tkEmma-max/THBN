dea0093867bf6ffac441115c9ce9f203ce37ccab
Tk <tkemma585@gmail.com>
2026-06-01 10:47:11 +0100

    commencement dr la page de register

---
+++ front-end/pages/register/main.js
@@ -0,0 +1,38 @@
+import { Header } from '/front-end/components/header/header.js';
+import { Footer } from '/front-end/components/footer/footer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const header = new Header();
+	header.renderHeader('head');
+	const footer = new Footer();
+	footer.renderFooter('foot');
+
+	addEventSwitch();
+});
+
+function switchBox(first_select, second_select) {
+	let first_elem = document.querySelector(first_select);
+	let second_elem = document.querySelector(second_select);
+
+	if (!first_elem || !second_elem) return;
+
+	first_elem.style.display = 'none';
+	second_elem.style.display = 'flex';
+}
+
+function addEventSwitch() {
+	let next = document.getElementById('next');
+	let previous = document.getElementById('previous');
+
+	if (next) {
+		next.addEventListener('click', () => {
+			switchBox('.first-register-page', '.second-register-page');
+		});
+	}
+
+	if (previous) {
+		previous.addEventListener('click', () => {
+			switchBox('.second-register-page', '.first-register-page');
+		});
+	}
+}
